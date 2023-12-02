4440645edbc10a1f2da3e422f56089e843753bd5
Marches <recettear.ho@gmail.com>
2023-12-02 12:37:15 +0000

    day 2 part 2

--- AdventOfCode2023/Day2/Solver.cs
+++ AdventOfCode2023/Day2/Solver.cs
@@ -5,17 +5,32 @@
         public void Run()
         {
             var reader = new CubeGameReader();
-            List<Game> games = File.ReadAllLines("day2_input.txt")
+            var games = File.ReadAllLines("day2_input.txt")
                 .Select(reader.Read)
+                .ToList();
+
+            //Part1(games);
+            Part2(games);
+        }
+
+        private void Part1(List<Game> games)
+        {
+            var sum = games
                 .Where(g => PossibleToContain(g, new CubeGroup(12, "red")))
                 .Where(g => PossibleToContain(g, new CubeGroup(13, "green")))
                 .Where(g => PossibleToContain(g, new CubeGroup(14, "blue")))
-                .ToList();
-
-            var sum = games
                 .Sum(g => g.Id);
 
             Console.WriteLine(sum);
+        }
+
+        private void Part2(List<Game> games)
+        {
+            var totalSetPower = games.Select(GetMinimalGroups)
+                .Select(SetPower)
+                .Sum();
+
+            Console.WriteLine(totalSetPower);
         }
 
         /// <summary>
@@ -28,6 +43,26 @@
                 .Where(s => s.Colour == group.Colour);
 
             return sameColour.All(c => c.Count <= group.Count);
+        }
+
+        /// <summary>
+        ///  The smallest cube groups that could have been used to create <paramref name="game"/>.
+        /// </summary>
+        /// <returns></returns>
+        private List<CubeGroup> GetMinimalGroups(Game game)
+        {
+            return game.Draws
+                .SelectMany(d => d.Groups)
+                .GroupBy(g => g.Colour)
+                .Select(x => x.MaxBy(g => g.Count))
+                .ToList();
+        }
+
+        private int SetPower(List<CubeGroup> cubeGroups)
+        {
+            return cubeGroups
+                .Skip(1)
+                .Aggregate(cubeGroups.First().Count, (total, next) => total * next.Count);
         }
     }
 }
