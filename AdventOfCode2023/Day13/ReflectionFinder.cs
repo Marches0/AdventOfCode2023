1b2b86a8724ef0e6eb4d3e6abf3612853fcc8779
Marches <recettear.ho@gmail.com>
2023-12-19 18:19:42 +0000

    day 13 part 2

--- AdventOfCode2023/Day13/ReflectionFinder.cs
+++ AdventOfCode2023/Day13/ReflectionFinder.cs
@@ -9,16 +9,23 @@ namespace Day13;
 
 internal class ReflectionFinder
 {
+    /// <summary>
+    ///  How many possible smudges we want to find. 0 = get exact match (part 1)
+    /// </summary>
+    private const int SmudgeCandidates = 1;
+
     public Reflection FindReflection(Valley valley)
     {
         ReflectionOrientation orientation = ReflectionOrientation.Row;
         int? reflection = FindRowReflection(valley.Content);
 
-        if(reflection is null)
+        if (reflection is null)
         {
             // rotate the contents so we can
             // reuse our logic
             orientation = ReflectionOrientation.Column;
+            
+            // number magically works despite the rotation!
             reflection = FindRowReflection(valley.Content.Rotate().ToList());
         }
 
@@ -34,8 +41,11 @@ internal class ReflectionFinder
         };
     }
 
-    private int? FindRowReflection(List<List<ValleyContent>> content)
+    private int? FindRowReflection(
+        List<List<ValleyContent>> content)
     {
+        List<(int, int)> differentIndexes = new();
+
         // Can't be a reflection if it's right on the end
         for (int i = 1; i < content.Count; i++)
         {
@@ -45,22 +55,63 @@ internal class ReflectionFinder
 
             // Both start from the reflection location being checked
             // and go out
-            var firstPart = content.Skip(firstStart)
+            List<List<ValleyContent>> firstPart = content.Skip(firstStart)
                 .Take(reflectionLength)
                 .Reverse()
                 .ToList();
 
-            var secondPart = content.Skip(i)
+            List<List<ValleyContent>> secondPart = content.Skip(i)
                 .Take(reflectionLength)
                 .ToList();
 
-            if (firstPart.Zip(secondPart).All(x => x.First.SequenceEqual(x.Second)))
+            // These indexes are local to firstPart and secondPart.
+            // firstPart is also REVERSED; ([last], 0) becomes ([first], 0)
+            // They need to be adjusted to be relative to content.
+            var localDifferences = GetDifferentPositions(firstPart, secondPart);
+            var actual = localDifferences
+                .Select(d => (reflectionLength - d.Item1 - 1, d.Item2))
+                .ToList();
+
+            if (actual.Count == SmudgeCandidates)
             {
                 return i;
             }
         }
 
         return null;
+    }
+
+    private List<(int, int)> GetDifferentPositions(
+        List<List<ValleyContent>> firstPart,
+        List<List<ValleyContent>> secondPart)
+    {
+        List<(int, int)> differentPositions = new();
+
+        for (int i = 0; i < firstPart.Count; i++)
+        {
+            // Get the number of differences between these two, and the positions
+            // those differences are in.
+            // Must be 
+            List<int> differences = GetDifferentIndexes(firstPart[i], secondPart[i]);
+            differentPositions.AddRange(differences.Select(d => (i, d)));
+        }
+
+        return differentPositions;
+    }
+
+    private List<int> GetDifferentIndexes(List<ValleyContent> a, List<ValleyContent> b)
+    {
+        List<int> different = new();
+
+        foreach (var (pair, i) in a.Zip(b).Select((pair, i) => (pair, i)))
+        {
+            if (pair.First != pair.Second)
+            {
+                different.Add(i);
+            }
+        }
+
+        return different;
     }
 }
 
